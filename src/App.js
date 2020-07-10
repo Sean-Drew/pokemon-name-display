adc60f96d60e35c3e51dd01ffb99e49c1020e3c6
Sean-Drew <SDrew989@gmail.com>
2020-07-10 11:48:26 -0400

    Add base webpage - App.js
    Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import React , { useState } from 'react'
+import React , { useState, useEffect } from 'react'
 import PokemonList from './PokemonList'
+import Pagination from './Pagination'
+import axios from 'axios'
 
 function App() {
-const [ pokemon, setPokemon ] = useState()
+  // Set some global variables - pokemon list, current & previous pages for pagination, loading state, etc.
+  const [ pokemon, setPokemon ] = useState([])
+  const [ currentPageUrl, setCurrentPageUrl ] = useState('https://pokeapi.co/api/v2/pokemon')
+  const [ nextPageUrl, setNextPageUrl ] = useState('https://pokeapi.co/api/v2/pokemon')
+  const [ prevPageUrl, setPrevPageUrl ] = useState('https://pokeapi.co/api/v2/pokemon')
+  // use loading states for a better user experience. default is set to true.
+  const [ loading, setLoading ] = useState(true)
+
+
+  useEffect(() => {
+    // for every request, set loading back to true until request resolves and response received
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      // create a way for old requests to be cancelled if they don't resolve before a new request
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    })
+      .then(res => {
+        // when request resolves and response received, loading can be set to false
+        setLoading(false)
+        // set the new 'next' and 'previous' pages
+        setNextPageUrl(res.data.next)
+        setPrevPageUrl(res.data.previous)
+        // set the new pokemon list
+        setPokemon(res.data.results.map(pokemon => pokemon.name))
+      })
+
+      // cancel old requests
+      return () => cancel()
+    // pass an argument below so that when the currentPageUrl changes, the page reloads by rerunning this axios GET call. If currentPageUrl doesn't change, page doesn't reload.
+  }, [currentPageUrl])
+
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function goToPrevPage() {
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  // use the loading state
+  if (loading) return 'Loading...'
 
   return (
+    <React.Fragment>
     <PokemonList pokemon={pokemon} />
+    <Pagination
+      goToNextPage={goToNextPage}
+      goToPrevPage={goToPrevPage}
+    />
+    </React.Fragment>
   )
 }
 
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function PokemonList({ pokemon }) {
   return (
     <div>
-      {pokemon.map(p => (
-        <div key={p}>{p}</div>
+      {pokemon.map(pokemon => (
+        <div key={pokemon}>{pokemon}</div>
       ))}
     </div>
   )
